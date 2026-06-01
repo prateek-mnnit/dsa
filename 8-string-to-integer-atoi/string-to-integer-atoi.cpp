c1ad47d702736b858e363e95687c55a720d0746b
Prateek <151605222+prateek-mnnit@users.noreply.github.com>
2026-06-01 12:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (20.64%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+        int sign = 1;
+        long long ans = 0;
+
+        while(i<n && s[i]==' '){
+            i++;
+        }
+
+        if(i<n && (s[i] == '-' || s[i] == '+')){
+            if(s[i] == '-'){
+                sign = -1;
+            }
+            i++;
+        }
+
+        while(i<n && isdigit(s[i])){
+            ans = ans * 10 + (s[i] - '0');
+             if(ans*sign>INT_MAX){
+            return INT_MAX;
+        }
+        if(ans*sign<INT_MIN){
+            return INT_MIN;
+        }
+
+            i++;
+        }
+        return ans*sign;
+    }
+};
