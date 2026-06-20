9059e97ec9dad538b25dc251fb6b750ae88ad0cc
Prateek <151605222+prateek-mnnit@users.noreply.github.com>
2026-06-20 18:40:53 +0530

    Time: 2251 ms (5.00%) | Memory: 8.6 MB (37.44%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,34 +1,49 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        
+        // Base case
+        if(dividend == divisor) return 1;
+        if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if(divisor == 1) return dividend;
+        
+        // Variable to store the sign of result
+        bool isPositive = true;
+        
+        // Updating the sign of quotient
+        if(dividend >= 0 && divisor < 0) 
+            isPositive = false;
+        else if(dividend < 0 && divisor > 0)
+            isPositive = false;
+            
+        // Storing absolute dividend & divisor
+        long long n = dividend;
+        long long d = divisor;
 
-        if (dividend == 0)
-            return 0;
-        if (divisor == 1)
-            return dividend;
-        if (divisor == -1) {
-            if (dividend == INT_MIN)
-                return INT_MAX;
-            return -dividend;
+        n = abs(n);
+        d = abs(d);
+        
+        // Variable to store the answer and sum
+        long long ans = 0, sum = 0;
+        
+        /* Looping while sum added to divisor is
+        less than or equal to divisor */
+        while(sum + d <= n) {
+            
+            // Increment the count
+           ans++;
+           // Update the sum
+           sum += d;
         }
-
-        long long dvd = llabs((long long)dividend);
-        long long dvs = llabs((long long)divisor);
-
-        long long count = 0;
-
-        while (dvd >= dvs) {
-            dvd -= dvs;
-            count++;
-        }
-
-        bool negative =
-            (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
-
-        if (negative) {
-            count = -count;
-        }
-
-        return (int)count;
+        
+        // Handling overflowing condition
+        if(ans > INT_MAX && isPositive) 
+            return INT_MAX;
+        if(ans > INT_MAX && !isPositive)
+            return INT_MIN;
+        
+        /* Returning the quotient 
+        with proper sign */
+        return isPositive ? ans : -1*ans;
     }
 };
