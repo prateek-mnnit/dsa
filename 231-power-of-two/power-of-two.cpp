97e033999b19071372c306acee52c5c9259752d2
Prateek <151605222+prateek-mnnit@users.noreply.github.com>
2026-06-20 15:03:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.11%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if( n<=0){
+            return false;
+        }
+        if (n == 1){
+            return true;
+        }   
+        
+        if(! (n & (n-1))){
+            return true;
+        }
+        
+            return false;
+    }
+};
