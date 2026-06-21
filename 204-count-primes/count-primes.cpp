f350cc8d9478d34ec676df7b9c81845171cdb90a
Prateek <151605222+prateek-mnnit@users.noreply.github.com>
2026-06-21 13:56:44 +0530

    Time: 227 ms (24.03%) | Memory: 132.5 MB (15.43%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPrime(int num, vector<int>& vec, int n){
         if(vec[num]==1){
-            for(int j=2*num; j<n; j+=num){
+            for(long j=(long)num*num; j<n; j+=num){
                 vec[j] = 0;
             }
             return true;
