af81dd57fd9160bf9f6d1ddbc16be15d10f577bc
Prateek <151605222+prateek-mnnit@users.noreply.github.com>
2026-09-08 16:41:35 +0530

    Time: 27 ms (67.40%) | Memory: 36.5 MB (57.57%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    int f(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp){
-        if(idx == 0){
-            if(amount % coins[0] == 0){
+    int f(int idx, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+        if (idx == 0) {
+            if (amount % coins[0] == 0) {
                 return 1;
-            }
-            else 
+            } else
                 return 0;
         }
-        if(amount == 0)
+        if (amount == 0)
             return 1;
 
-        if(dp[idx][amount] != -1)  return dp[idx][amount];
+        if (dp[idx][amount] != -1)
+            return dp[idx][amount];
 
-        int notTake = f(idx-1,amount,coins,dp);
+        int notTake = f(idx - 1, amount, coins, dp);
         int take = 0;
-        if(amount >= coins[idx])
-            take = f(idx,amount-coins[idx],coins,dp);
-        
+        if (amount >= coins[idx])
+            take = f(idx, amount - coins[idx], coins, dp);
+
         return dp[idx][amount] = take + notTake;
     }
+
+    // int tabulation(int n, int amount, vector<int>& coins) {
+    //     vector<vector<long long>> dp(n, vector<long long>(amount + 1, 0));
+
+    //     for (int i = 0; i <= amount; i++) {
+    //         if (i % coins[0] == 0) {
+    //             dp[0][i] = 1;
+    //         }
+    //     }
+
+    //     for (int idx = 1; idx < n; idx++) {
+    //         for (int target = 0; target <= amount; target++) {
+    //             long long notTake = dp[idx - 1][target];
+    //             long long take = 0;
+    //             if (target >= coins[idx])
+    //                 take = dp[idx][target - coins[idx]];
+
+    //             dp[idx][target] = take + notTake;
+    //         }
+    //     }
+    //     return (int)dp[n - 1][amount];
+    // }
+
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n, vector<int>(amount+1,-1));
-        return f(n-1,amount,coins,dp); 
+        return f(n-1,amount,coins,dp);
+        // return tabulation(n, amount, coins);
     }
 };
